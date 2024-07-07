cc79b80b2ed1d975663286ab97ac60bc6ebeef16
dyguests <dyguests@qq.com>
2024-07-07 13:32:18 +0800

    game hint

--- Assets/Scripts/Repositories/GameDatas.cs
+++ Assets/Scripts/Repositories/GameDatas.cs
@@ -16,11 +16,27 @@ namespace Repositories
             return index switch
             {
                 0 => GetLevel0(),
+                1 => GetLevel1(),
                 _ => GetLevel0()
             };
         }
 
         private static IGame GetLevel0()
+        {
+            var plate = new Plate(7, 5);
+            plate.Size.GetEnumerator().ForEach(pos => plate.Insert(pos, new Ground()));
+            plate.Insert(new Vector2Int(1, 1), new Player());
+            plate.Insert(new Vector2Int(2, 2), new Box());
+            plate.Insert(new Vector2Int(3, 1), new Wall());
+            plate.Insert(new Vector2Int(3, 2), new Wall());
+            plate.Insert(new Vector2Int(5, 2), new Goal());
+            var game = new Game(plate);
+            game.Record();
+
+            return game;
+        }
+
+        private static IGame GetLevel1()
         {
             var plate = new Plate(7, 5);
             plate.Size.GetEnumerator().ForEach(pos => plate.Insert(pos, new Ground()));
