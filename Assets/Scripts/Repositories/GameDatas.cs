abcdea9ab48d97bec53135368e1272d26b661945
dyguests <dyguests@qq.com>
2024-07-07 13:35:42 +0800

    两个关卡

--- Assets/Scripts/Repositories/GameDatas.cs
+++ Assets/Scripts/Repositories/GameDatas.cs
@@ -7,9 +7,9 @@ namespace Repositories
     /// <summary>
     ///  关卡数据
     /// </summary>
-    public class GameDatas
+    public static class GameDatas
     {
-        public const int Count = 1;
+        public const int Count = 2;
 
         public static IGame GetLevel(int index)
         {
@@ -42,9 +42,9 @@ namespace Repositories
             plate.Size.GetEnumerator().ForEach(pos => plate.Insert(pos, new Ground()));
             plate.Insert(new Vector2Int(1, 1), new Player());
             plate.Insert(new Vector2Int(2, 2), new Box());
-            plate.Insert(new Vector2Int(3, 1), new Splitter(false, true, false, true));
-            plate.Insert(new Vector2Int(3, 3), new Wall());
-            plate.Insert(new Vector2Int(4, 2), new Goal());
+            plate.Insert(new Vector2Int(3, 2), new Splitter(false, true, false, true));
+            plate.Insert(new Vector2Int(4, 2), new Wall());
+            plate.Insert(new Vector2Int(5, 2), new Goal());
             var game = new Game(plate);
             game.Record();
 
