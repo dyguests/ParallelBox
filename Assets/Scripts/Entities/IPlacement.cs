d82fd9eca4b1d46f794235b57ea1d8bc28b786e2
dyguests <dyguests@qq.com>
2024-07-07 14:01:21 +0800

    占比

--- Assets/Scripts/Entities/IPlacement.cs
+++ Assets/Scripts/Entities/IPlacement.cs
@@ -9,7 +9,15 @@ namespace Entities
     public interface IPlacement : IRecordable, IDeepCloneable<IPlacement>
     {
         [RecordlessField] public IPlate Plate { get; }
+
         Vector2Int Pos { get; set; }
+
+        /**
+         * this在世界中的占比
+         * 占比 = Ratio.x/Ratio.y
+         * Ratio.z = 0 表示不可变，1 表示跟随世界分裂而修改占比
+         */
+        Vector3Int Ratio { get; set; }
 
         /// <summary>
         /// 在同一个Pos能放多个IPlacement，且一种Level仅能放置一个
@@ -35,7 +43,9 @@ namespace Entities
         #region IPlacement
 
         public IPlate Plate { get; private set; }
+
         public Vector2Int Pos { get; set; }
+        public Vector3Int Ratio { get; set; } = new Vector3Int(1, 1, 0);
         public virtual int Layer => 0;
 
         public void Inserted(IPlate plate, Vector2Int pos)
@@ -58,6 +68,7 @@ namespace Entities
         protected static void PlacementDeepClone(IPlacement source, IPlacement target)
         {
             ((Placement)target).Plate = source.Plate;
+            target.Ratio = source.Ratio; // todo 分裂
             target.Pos = source.Pos;
         }
 
